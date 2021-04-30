324e8953a809c0632a3a5420a7ef0f195e6e791e
Dwaraka-Poreddy <59866030+Dwaraka-Poreddy@users.noreply.github.com>
2021-05-01 01:53:31 +0530

    Add files via upload
    Working Google Clone

---
+++ build/precache-manifest.6aa2c13b3d624e4b7e7c9ae7fb79715e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63a816e82bc079abc541bb5037fc69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40395d505ad8b66d3ba6",
+    "url": "/static/css/main.1f367221.chunk.css"
+  },
+  {
+    "revision": "2679ca319e2094235dfd",
+    "url": "/static/js/2.da850c9a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.da850c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40395d505ad8b66d3ba6",
+    "url": "/static/js/main.e5264f13.chunk.js"
+  },
+  {
+    "revision": "9eb3a9a40513b5c83cbc",
+    "url": "/static/js/runtime-main.bb6addc8.js"
+  }
+]);
